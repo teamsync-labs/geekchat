89be4cf5d0fcb27f9b074973974300922fd67cf2
Илья <ilialugovskoy2011@gmail.com>
2026-07-14 15:48:38 +0300

    chore: remove duplicate GeekChat folder from frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './index.css';
-import Chat from './components/Chat';
+import CallScreen from './components/CallScreen';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-[#0A1628] flex items-center justify-center">
-      <Chat />
-    </div>
-  );
+  return <CallScreen />;
 }
 
 export default App;
