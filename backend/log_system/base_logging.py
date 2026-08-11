63e8001d21d4171b0e8a43e3dd29606e1035dd62
zaharsamborskiy <117350706+zaharsamborskiy@users.noreply.github.com>
2026-08-11 21:41:35 +0700

    Фикс по ревью: Перенос логик в базовый класс,async метод, использование LogLevel

--- backend/log_system/base_logging.py
+++ backend/log_system/base_logging.py
@@ -1,14 +1,44 @@
 from typing import Optional
+import logging
 from abc import ABC, abstractmethod
+from pathlib import Path
+from backend.core.program_codes import LogLevel as l
 
 
 class BaseLoggingEntity(ABC):
+    def __init__(self) -> None:
+        log_dir = Path('logs')
+        log_file_path = log_dir / 'program_log.log'
+        log_dir.mkdir(parents=True, exist_ok=True)
 
+        logging.basicConfig(
+            filename=log_file_path,
+            level=logging.INFO,
+            format=f'[ %(asctime)s - {self.get_current_entity()} - %(levelname)s - %(message)s ]',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            encoding='utf-8'
+        )
 
     @abstractmethod
-    def create_log_line(
-            self,
-            internal_error_code: str,
-            comment: str,
-            web_code: Optional[str] = 'not required') -> None:
+    def get_current_entity(self):
         pass
+
+    @staticmethod
+    async def create_log_line(
+        internal_error_code,
+        log_level: str,
+        description: Optional[str] = 'None',
+        web_code: Optional[str] = 'not required'
+    ):
+
+        message = f'{internal_error_code} - {description} - {description} - web_code: {web_code}'
+
+        match log_level.upper():
+            case l.INFO:
+                logging.info(message)
+            case l.WARNING:
+                logging.warning(message)
+            case l.ERROR:
+                logging.error(message)
+            case l.CRITICAL:
+                logging.critical(message)
